8de37a88f4ef1c7acea868f817b21d2fa44891ea
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-01 21:52:03 +0100

    Refactor and add solution to dec_01, part two.

--- src/dec_01.rs
+++ src/dec_01.rs
@@ -1,24 +1,42 @@
 use std::collections::HashSet;
-use std::io::{self, BufRead};
-use std::path::Path;
 
 use crate::common;
 
+const YEAR: i32 = 2020;
+
 pub fn part_one() {
+    println!("--- Part One ---");
+
     let numbers = common::read_numbers("./data/dec_01.txt");
-    let mut numbers_set = HashSet::new();
-    for number in numbers {
-        let inverse = 2020 - number;
-        if numbers_set.contains(&inverse) {
-            let result = number * inverse;
-            println!("{}", result);
-            return;
-        } else {
-            numbers_set.insert(number);
-        }
+    let addends = find_addends(&numbers, YEAR);
+    match addends {
+        Some(a) => println!("Result: {} + {} = {}, {} * {} = {}", a.0, a.1, a.0 + a.1, a.0, a.1, a.0 * a.1),
+        None => println!("Count not find addends."),
     }
 }
 
 pub fn part_two() {
+    println!("--- Part Two ---");
 
+    let numbers = common::read_numbers("./data/dec_01.txt");
+    for number in &numbers {
+        let addends = find_addends(&numbers, YEAR - number);
+        match addends {
+            Some(s) => println!("Result: {} + {} + {} = {}, {} * {} * {} = {}", number, s.0, s.1, number + s.0 + s.1, number, s.0, s.1, number * s.0 * s.1),
+            None => {}
+        }
+    }
+}
+
+fn find_addends(numbers: &Vec<i32>, sum: i32) -> Option<(i32, i32)> {
+    let mut numbers_set = HashSet::new();
+    for number in numbers {
+        let complement = sum - number;
+        if numbers_set.contains(&complement) {
+            return Some((*number, complement));
+        } else {
+            numbers_set.insert(number);
+        }
+    }
+    return None;
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod dec_01;
 mod common;
 
 fn main() {
+    println!("December 1st, 2020");
     dec_01::part_one();
     dec_01::part_two();
 }
