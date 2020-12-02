da96caaca31a3a86bd8b4a9a2278dcaaa77c9e56
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-02 13:11:51 +0100

    Add solutions to dec_02, part one & two.

--- src/common.rs
+++ src/common.rs
@@ -16,6 +16,19 @@ pub fn read_numbers(filename: &str) -> Vec<i32> {
     return numbers;
 }
 
+pub fn read_strings(filename: &str) -> Vec<String> {
+    let mut strings = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(l) = line {
+                strings.push(l);
+            }
+        }
+    }
+    return strings;
+}
+
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> where P: AsRef<Path>, {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod dec_01;
+mod dec_02;
 mod common;
 
 fn main() {
-    println!("December 1st, 2020");
+    println!("\nDecember 1st, 2020");
     dec_01::part_one();
     dec_01::part_two();
+
+    println!("\nDecember 2nd, 2020");
+    dec_02::part_one();
+    dec_02::part_two();
 }
