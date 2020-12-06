4b4bc1ae176b7ae22fcdd794f902ab8d7a8ae327
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-06 14:21:35 +0100

    Refactor.

--- src/common.rs
+++ src/common.rs
@@ -29,6 +29,29 @@ pub fn read_strings(filename: &str) -> Vec<String> {
     return strings;
 }
 
+// read chunks of lines separated by empty lines
+pub fn read_chunks(filename: &str) -> Vec<Vec<String>> {
+    let mut chunks = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        // Consumes the iterator, returns an (Optional) String
+        let mut strings = Vec::new();
+        for line in lines {
+            if let Ok(l) = line {
+                if l.trim().is_empty() {
+                    chunks.push(strings);
+                    strings = Vec::new();
+                } else {
+                    strings.push(l);
+                }
+            }
+        }
+        if strings.len() > 0 {
+            chunks.push(strings);
+        }
+    }
+    return chunks;
+}
+
 pub fn format_to_sum(numbers: &Vec<i32>) -> String {
     let strings: Vec<String> = numbers.iter().map(|n| n.to_string()).collect();
     let joined = strings.join(" + ");
--- src/dec_06.rs
+++ src/dec_06.rs
@@ -5,21 +5,16 @@ use crate::common;
 pub fn part_one() {
     println!("--- Part One ---");
 
-    let lines = common::read_strings("./data/dec_06.txt");
-    let mut sum = 0;
-    let mut set: HashSet<char> = HashSet::new();
-    for line in lines {
-        if line.trim().is_empty() {
-            sum += set.len();
-            set.clear();
-        } else {
-            for c in line.chars() {
-                set.insert(c);
+    let sum: i32 = common::read_chunks("./data/dec_06.txt").iter()
+        .map(|chunk| {
+            let mut set: HashSet<char> = HashSet::new();
+            for line in chunk {
+                for c in line.chars() {
+                    set.insert(c);
+                }
             }
-        }
-    }
-    sum += set.len();
-    set.clear();
+            return set.len() as i32;
+        }).sum();
 
     println!("Result: {}", sum);
 }
@@ -27,26 +22,18 @@ pub fn part_one() {
 pub fn part_two() {
     println!("--- Part Two ---");
 
-    let lines = common::read_strings("./data/dec_06.txt");
+    let chunks = common::read_chunks("./data/dec_06.txt");
     let mut sum = 0;
-    let mut count = 0;
-    let mut map: HashMap<char, i32> = HashMap::new();
-    for line in lines {
-        if line.trim().is_empty() {
-            sum += map.iter().filter(|(&key,&value)|value == count).count();
-            map.clear();
-            count = 0;
-        } else {
+    for chunk in chunks {
+        let mut map: HashMap<char, i32> = HashMap::new();
+        let chunk_len = chunk.len() as i32;
+        for line in chunk {
             for c in line.chars() {
                 map.insert(c, map.get(&c).unwrap_or(&0) + 1);
             }
-            count += 1;
         }
-
+        sum += map.iter().filter(|(&_, &value)| value == chunk_len).count();
     }
-    sum += map.iter().filter(|(&key,&value)|value == count).count();
-    map.clear();
-    count = 0;
 
     println!("Result: {}", sum);
 }
