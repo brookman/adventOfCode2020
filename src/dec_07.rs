de1e0234867ebab1846af1236dc1af873616080d
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-10 21:19:57 +0100

    Add solutions to dec_07, part two.

--- src/dec_07.rs
+++ src/dec_07.rs
@@ -13,19 +13,15 @@ struct Bags {
 
 #[derive(Clone, Debug)]
 struct Bag {
+    index: usize,
     color: String,
-    parents: Vec<ParentConnection>,
+    parents: Vec<BagWithQuantity>,
+    children: Vec<BagWithQuantity>,
 }
 
 #[derive(Clone, Debug)]
-struct ParentConnection {
+struct BagWithQuantity {
     bag_index: usize,
-    quantity: i32,
-}
-
-#[derive(Clone, Debug)]
-struct ChildBag {
-    color: String,
     quantity: i32,
 }
 
@@ -41,73 +37,92 @@ impl Bags {
         let mut list: Vec<Bag> = Vec::new();
 
         for string in strings {
-            if PRIMARY.is_match(&string) {
-                let caps = PRIMARY.captures(&string).unwrap();
-                let color = caps[1].to_string();
-                let rest = caps[2].to_string();
-                let child_strings = rest.split(",").map(|c| c.trim()).collect::<Vec<&str>>();
-                let mut children: Vec<ChildBag> = Vec::new();
-
-                for child_string in child_strings {
-                    if CHILD.is_match(child_string) {
-                        let caps = CHILD.captures(child_string).unwrap();
-                        let quantity = caps[1].to_string().parse::<i32>().unwrap();
-                        let child_color = caps[2].to_string();
-                        children.push(ChildBag {
-                            color: child_color,
-                            quantity,
-                        });
-                    } else if CHILD_ZERO.is_match(child_string) {} else {
-                        panic!("error: {}", child_string);
-                    }
-                }
-
-                let parent_index: usize;
-                if map.contains_key(&color) {
-                    parent_index = *map.get(&color).unwrap();
-                } else {
-                    list.push(Bag {
-                        color: color.clone(),
-                        parents: vec![],
-                    });
-                    map.insert(color.clone(), list.len() - 1);
-                    parent_index = list.len() - 1;
-                }
-
-                for child in children {
-                    let child_index: usize;
-                    if map.contains_key(&child.color) {
-                        child_index = *map.get(&child.color).unwrap();
-                    } else {
-                        list.push(Bag {
-                            color: child.color.clone(),
-                            parents: vec![],
-                        });
-                        map.insert(child.color.clone(), list.len() - 1);
-                        child_index = list.len() - 1;
-                    }
-                    let old_child = &list[child_index];
-                    let mut parents: Vec<ParentConnection> = Vec::new();
-                    parents.extend(old_child.parents.to_vec());
-                    parents.push(ParentConnection {
-                        quantity: child.quantity,
-                        bag_index: parent_index,
-                    });
-
-                    let new_child = Bag {
-                        color: old_child.color.clone(),
-                        parents,
-                    };
-                    list[child_index] = new_child;
-                }
-            } else {
-                panic!();
+            if !PRIMARY.is_match(&string) {
+                panic!("error: {}", string);
             }
+            let caps = PRIMARY.captures(&string).unwrap();
+            let color = caps[1].to_string();
+            let rest = caps[2].to_string();
+            let child_strings = rest.split(",").map(|c| c.trim()).collect::<Vec<&str>>();
+
+            let result = Bags::insert(map, list, &color);
+            map = result.0;
+            list = result.1;
+            let parent = result.2;
+
+            // let parent = list[parent_index].clone();
+            let mut children: Vec<BagWithQuantity> = Vec::new();
+            children.extend(parent.children.to_vec());
+
+            for child_string in child_strings {
+                if CHILD_ZERO.is_match(child_string) {
+                    continue;
+                }
+                if !CHILD.is_match(child_string) {
+                    panic!("error: {}", child_string);
+                }
+
+                let caps = CHILD.captures(child_string).unwrap();
+                let quantity = caps[1].to_string().parse::<i32>().unwrap();
+                let child_color = caps[2].to_string();
+
+                let result = Bags::insert(map, list, &child_color);
+                map = result.0;
+                list = result.1;
+                let child = result.2;
+
+                children.push(BagWithQuantity {
+                    quantity,
+                    bag_index: child.index,
+                });
+
+                // let child = &list[child_index];
+                let mut parents: Vec<BagWithQuantity> = Vec::new();
+                parents.extend(child.parents.to_vec());
+                parents.push(BagWithQuantity {
+                    quantity,
+                    bag_index: parent.index,
+                });
+
+                list[child.index] = Bag {
+                    index: child.index,
+                    color: child.color.clone(),
+                    parents,
+                    children: child.children.clone(),
+                };
+            }
+
+            list[parent.index] = Bag {
+                index: parent.index,
+                color: parent.color.clone(),
+                parents: parent.parents.clone(),
+                children,
+            };
         }
         return Bags {
             map,
             list,
         };
+    }
+
+    fn insert(mut map: HashMap<String, usize>, mut list: Vec<Bag>, color: &String) -> (HashMap<String, usize>, Vec<Bag>, Bag) {
+        let index: usize;
+        let bag: Bag;
+        if map.contains_key(color) {
+            index = *map.get(color).unwrap();
+        } else {
+            index = list.len();
+            list.push(Bag {
+                index,
+                color: color.clone(),
+                parents: vec![],
+                children: vec![],
+            });
+            map.insert(color.clone(), index);
+        }
+        bag = list[index].clone();
+
+        return (map, list, bag);
     }
 
     fn count_number_of_unique_parents_for_color(&self, color: &String) -> usize {
@@ -127,10 +142,28 @@ impl Bags {
 
         return indices;
     }
+
+    fn count_number_of_cumulative_children_for_color(&self, color: &String) -> i32 {
+        let start_index = self.map.get(color).unwrap();
+        println!("start_index {}", start_index);
+        return self.count_number_of_cumulative_children_for_index(*start_index) - 1;
+    }
+
+    fn count_number_of_cumulative_children_for_index(&self, index: usize) -> i32 {
+        let bag: &Bag = self.list.get(index).unwrap();
+
+        let mut total = 1;
+        for child in &bag.children {
+            total += child.quantity * self.count_number_of_cumulative_children_for_index(child.bag_index);
+        }
+
+        return total;
+    }
 }
 
 pub fn part_one() {
     println!("--- Part One ---");
+
     let bags: Bags = Bags::parse(common::read_strings("./data/dec_07.txt"));
     let count = bags.count_number_of_unique_parents_for_color(&"shiny gold".to_string());
     println!("Result: {}", count);
@@ -138,4 +171,8 @@ pub fn part_one() {
 
 pub fn part_two() {
     println!("--- Part Two ---");
+
+    let bags: Bags = Bags::parse(common::read_strings("./data/dec_07.txt"));
+    let count = bags.count_number_of_cumulative_children_for_color(&"shiny gold".to_string());
+    println!("Result: {}", count);
 }
