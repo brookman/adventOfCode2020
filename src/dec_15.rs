07e2143bdef1a6ecc6125039799d3524cbc4c03c
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-19 17:17:36 +0100

    Add solutions to dec_15, speed improvements.

--- src/dec_15.rs
+++ src/dec_15.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::collections::HashMap;
 
 use crate::common;
@@ -11,9 +12,9 @@ pub fn part_one() {
 
 pub fn part_two() {
     println!("--- Part Two ---");
-
     let numbers: Vec<i32> = parse_numbers();
-    println!("Result: {:?}", get_nth_number(&numbers, 30_000_000));
+    let result = get_nth_number(&numbers, 30_000_000);
+    println!("Result: {:?}", result);
 }
 
 fn parse_numbers() -> Vec<i32> {
@@ -23,30 +24,17 @@ fn parse_numbers() -> Vec<i32> {
         .collect();
 }
 
-fn get_nth_number(starting_numbers: &Vec<i32>, n: i32) -> i32 {
-    let mut map: Vec<i32> = vec![-1; n as usize];
-
-    let mut last = *starting_numbers.last().unwrap();
-    let mut count = 0;
-
-    for n in starting_numbers {
-        map[*n as usize] = count;
-        last = *n;
-        count += 1;
+fn get_nth_number(initial: &Vec<i32>, n: i32) -> i32 {
+    let mut map: Vec<i32> = vec![0; n as usize];
+    let mut state = 0 as i32;
+    for i in 0..initial.len() as i32 {
+        map[state as usize] = i;
+        state = initial[i as usize];
     }
-
-    let max = n - starting_numbers.len() as i32;
-    for _ in 0..max {
-        let mut number = 0;
-        let val = map[last as usize];
-        if val > -1 {
-            number = count - val - 1;
-        }
-
-        map[last as usize] = count - 1;
-        last = number;
-        count += 1;
+    for i in (initial.len() as i32)..n {
+        let v = map[state as usize];
+        map[state as usize] = i;
+        state = (i - v) * cmp::min(v, 1);
     }
-
-    return last;
+    return state;
 }
