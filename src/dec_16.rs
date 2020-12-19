a7e95c58a54aee043ff5fc8b00594f813a45f1b4
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-19 17:24:03 +0100

    Prepare dec_16 - dec_19

---
+++ src/dec_16.rs
@@ -0,0 +1,15 @@
+use crate::common;
+
+pub fn part_one() {
+    println!("--- Part One ---");
+
+    let result = 0;
+    println!("Result: {:?}", result);
+}
+
+pub fn part_two() {
+    println!("--- Part Two ---");
+
+    let result = 0;
+    println!("Result: {:?}", result);
+}
--- src/main.rs
+++ src/main.rs
@@ -13,67 +13,87 @@ mod dec_12;
 mod dec_13;
 mod dec_14;
 mod dec_15;
+mod dec_16;
+mod dec_17;
+mod dec_18;
+mod dec_19;
 mod common;
 mod vectors;
 
 fn main() {
-    println!("\nDecember 1st, 2020");
-    dec_01::part_one();
-    dec_01::part_two();
+    // println!("\nDecember 1st, 2020");
+    // dec_01::part_one();
+    // dec_01::part_two();
+    //
+    // println!("\nDecember 2nd, 2020");
+    // dec_02::part_one();
+    // dec_02::part_two();
+    //
+    // println!("\nDecember 3rd, 2020");
+    // dec_03::part_one();
+    // dec_03::part_two();
+    //
+    // println!("\nDecember 4th, 2020");
+    // dec_04::part_one();
+    // dec_04::part_two();
+    //
+    // println!("\nDecember 5th, 2020");
+    // dec_05::part_one();
+    // dec_05::part_two();
+    //
+    // println!("\nDecember 6th, 2020");
+    // dec_06::part_one();
+    // dec_06::part_two();
+    //
+    // println!("\nDecember 7th, 2020");
+    // dec_07::part_one();
+    // dec_07::part_two();
+    //
+    // println!("\nDecember 8th, 2020");
+    // dec_08::part_one();
+    // dec_08::part_two();
+    //
+    // println!("\nDecember 9th, 2020");
+    // dec_09::part_one();
+    // dec_09::part_two();
+    //
+    // println!("\nDecember 10th, 2020");
+    // dec_10::part_one();
+    // dec_10::part_two();
+    //
+    // println!("\nDecember 11th, 2020");
+    // dec_11::part_one();
+    // dec_11::part_two();
+    //
+    // println!("\nDecember 12th, 2020");
+    // dec_12::part_one();
+    // dec_12::part_two();
+    //
+    // println!("\nDecember 13th, 2020");
+    // dec_13::part_one();
+    // dec_13::part_two();
+    //
+    // println!("\nDecember 14th, 2020");
+    // dec_14::part_one();
+    // dec_14::part_two();
+    //
+    // println!("\nDecember 15th, 2020");
+    // dec_15::part_one();
+    // dec_15::part_two();
 
-    println!("\nDecember 2nd, 2020");
-    dec_02::part_one();
-    dec_02::part_two();
+    println!("\nDecember 16th, 2020");
+    dec_16::part_one();
+    dec_16::part_two();
 
-    println!("\nDecember 3rd, 2020");
-    dec_03::part_one();
-    dec_03::part_two();
+    println!("\nDecember 17th, 2020");
+    dec_17::part_one();
+    dec_17::part_two();
 
-    println!("\nDecember 4th, 2020");
-    dec_04::part_one();
-    dec_04::part_two();
+    println!("\nDecember 18th, 2020");
+    dec_18::part_one();
+    dec_18::part_two();
 
-    println!("\nDecember 5th, 2020");
-    dec_05::part_one();
-    dec_05::part_two();
-
-    println!("\nDecember 6th, 2020");
-    dec_06::part_one();
-    dec_06::part_two();
-
-    println!("\nDecember 7th, 2020");
-    dec_07::part_one();
-    dec_07::part_two();
-
-    println!("\nDecember 8th, 2020");
-    dec_08::part_one();
-    dec_08::part_two();
-
-    println!("\nDecember 9th, 2020");
-    dec_09::part_one();
-    dec_09::part_two();
-
-    println!("\nDecember 10th, 2020");
-    dec_10::part_one();
-    dec_10::part_two();
-
-    println!("\nDecember 11th, 2020");
-    dec_11::part_one();
-    dec_11::part_two();
-
-    println!("\nDecember 12th, 2020");
-    dec_12::part_one();
-    dec_12::part_two();
-
-    println!("\nDecember 13th, 2020");
-    dec_13::part_one();
-    dec_13::part_two();
-
-    println!("\nDecember 14th, 2020");
-    dec_14::part_one();
-    dec_14::part_two();
-
-    println!("\nDecember 15th, 2020");
-    dec_15::part_one();
-    dec_15::part_two();
+    println!("\nDecember 19th, 2020");
+    dec_19::part_one();
+    dec_19::part_two();
 }
