4d93e31166112b5da5ef3dcd453f99aa3919fbb4
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2020-12-24 10:18:07 +0100

    Refactor.

--- src/dec_22.rs
+++ src/dec_22.rs
@@ -2,38 +2,28 @@ use crate::common;
 
 pub fn part_one() {
     println!("--- Part One ---");
+
     let data = common::read_strings("./data/dec_22.txt");
     let mut player1 = data[1..26].iter().rev().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>();
     let mut player2 = data[28..].iter().rev().map(|n| n.parse::<i32>().unwrap()).collect::<Vec<i32>>();
 
-    println!("player1: {:?}", player1);
-    println!("player2: {:?}", player2);
+    while !player1.is_empty() && !player2.is_empty() {
+        let card1 = player1.pop().unwrap();
+        let card2 = player2.pop().unwrap();
 
-    loop {
-        let card1 = player1.pop();
-        let card2 = player2.pop();
-
-        if card1.unwrap() > card2.unwrap() {
-            player1.insert(0, card1.unwrap());
-            player1.insert(0, card2.unwrap());
+        if card1 > card2 {
+            player1.insert(0, card1);
+            player1.insert(0, card2);
         } else {
-            player2.insert(0, card2.unwrap());
-            player2.insert(0, card1.unwrap());
+            player2.insert(0, card2);
+            player2.insert(0, card1);
         }
+    }
 
-        println!("player1: {:?}", player1);
-        println!("player2: {:?}", player2);
-
-        if player1.is_empty() {
-            println!("player2 won");
-            print_result(&player2);
-            break;
-        }
-        if player2.is_empty() {
-            println!("player1 won");
-            print_result(&player1);
-            break;
-        }
+    if player1.is_empty() {
+        print_result(&player2);
+    } else if player2.is_empty() {
+        print_result(&player1);
     }
 }
 
