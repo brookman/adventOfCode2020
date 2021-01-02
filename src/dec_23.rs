46f3706dae9a2f7d3b4c57c4e2671811aa80506e
Bachmann, Benjamin <Benjamin.Bachmann@zuehlke.com>
2021-01-02 15:52:10 +0100

    Refactor dec_23.

--- src/dec_23.rs
+++ src/dec_23.rs
@@ -6,8 +6,9 @@ struct Element {
     prev: usize,
 }
 
-fn wrap(i: i32, max_u: usize) -> usize {
-    let max = max_u as i32;
+#[inline(always)]
+fn wrap(i: i64, max_u: usize) -> usize {
+    let max = max_u as i64;
     (((i % max) + max) % max) as usize
 }
 
@@ -15,9 +16,9 @@ fn init(arr: &[usize]) -> Vec<Element> {
     let len = arr.len();
     let mut elements: Vec<Element> = vec![Element { prev: 0, next: 0 }; len];
     for (i, v) in arr.iter().enumerate() {
-        let index = wrap(*v as i32 - 1, len);
-        let next = arr[wrap(i as i32 + 1, len)] - 1;
-        let prev = arr[wrap(i as i32 - 1, len)] - 1;
+        let index = wrap(*v as i64 - 1, len);
+        let next = arr[wrap(i as i64 + 1, len)] - 1;
+        let prev = arr[wrap(i as i64 - 1, len)] - 1;
         elements[index] = Element { next, prev };
     }
     elements
@@ -26,7 +27,12 @@ fn init(arr: &[usize]) -> Vec<Element> {
 pub fn part_one() {
     println!("--- Part One ---");
 
-    let cups = run(&[1, 5, 6, 7, 9, 4, 8, 2, 3], 100);
+    let numbers = common::read_strings("./data/dec_23.txt")[0]
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect::<Vec<usize>>();
+
+    let cups = run(&numbers, 100);
 
     let mut r = 0;
     let mut result_vec: Vec<usize> = vec![];
@@ -42,8 +48,8 @@ pub fn part_one() {
 pub fn part_two() {
     println!("--- Part Two ---");
 
-    let mut arr = vec![3, 8, 9, 1, 2, 5, 4, 6, 7];
-    for n in (arr.len() + 1)..1_000_000 {
+    let mut arr = vec![1, 5, 6, 7, 9, 4, 8, 2, 3];
+    for n in (arr.len() + 1)..=1_000_000 {
         arr.push(n);
     }
 
@@ -57,6 +63,7 @@ pub fn part_two() {
 
 fn run(arr: &[usize], iterations: usize) -> Vec<Element> {
     let mut cups = init(&arr);
+
     let mut c0 = arr[0] - 1;
 
     for _ in 0..iterations {
@@ -64,12 +71,13 @@ fn run(arr: &[usize], iterations: usize) -> Vec<Element> {
         let c2 = cups[c1].next;
         let c3 = cups[c2].next;
         let c4 = cups[c3].next;
+
         cups[c0].next = c4;
         cups[c4].prev = c0;
 
-        let mut dest = wrap(c0 as i32 - 1, cups.len());
+        let mut dest = wrap(c0 as i64 - 1, cups.len());
         while dest == c1 || dest == c2 || dest == c3 {
-            dest = wrap(dest as i32 - 1, cups.len());
+            dest = wrap(dest as i64 - 1, cups.len());
         }
         let dest_next = cups[dest].next;
 
